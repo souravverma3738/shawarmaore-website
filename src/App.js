12ff6a8b7a740e06add5785457bf12087a1424f8
Administrator <souravverma1582000@gmail.com>
2025-11-30 22:02:59 +0000

    push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import AdminOrders from "./pages/AdminOrders";
 import AdminSales from "./pages/AdminSales";
 import { Toaster } from "./components/sonner";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = "http://127.0.0.1:5000";
 export const API = `${BACKEND_URL}/api`;
 
 // Auth Context
